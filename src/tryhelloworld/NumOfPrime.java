9922624a0afc28098e3de56cede068a2e9aabe3f
soulduse <soulduse@naver.com>
2016-05-14 17:36:34 +0900

    소수구하기

---
+++ src/tryhelloworld/NumOfPrime.java
@@ -0,0 +1,26 @@
+package tryhelloworld;
+
+/**
+ * Created by sould on 2016-05-14.
+ */
+public class NumOfPrime {
+    int numberOfPrime(int n) {
+        int result = 0;
+        // 함수를 완성하세요.
+
+        for(int i=2; i<=n; i++){
+           for(int j=2; j<=i; j++){
+               if(i!=j && i%j==0)break;
+               if(i==j){
+                   result++;
+               }
+           }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        NumOfPrime prime = new NumOfPrime();
+        System.out.println( prime.numberOfPrime(10) );
+    }
+}
