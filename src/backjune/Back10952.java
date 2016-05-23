3e47285e349d7857efc2e1e62d7a9e77400f8025
soulduse <soulduse@naver.com>
2016-05-24 00:08:14 +0900

    백준저지 10952

---
+++ src/backjune/Back10952.java
@@ -0,0 +1,17 @@
+package backjune;
+
+import java.util.Scanner;
+
+/**
+ * Created by sould on 2016-05-24.
+ */
+public class Back10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = sc.nextInt()+sc.nextInt();
+        while(sum!=0){
+            System.out.println(sum);
+            sum = sc.nextInt()+sc.nextInt();
+        }
+    }
+}
